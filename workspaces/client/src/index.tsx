394b8504fb1b18ab61583b17f3f428339f48dc5f
seitamuro <seitamuro0210@gmail.com>
2024-03-26 10:34:35 +0900

    feat(client): クライアント側で表示するページを作成

---
+++ workspaces/client/src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>hello world</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ workspaces/client/tsup.config.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+
+import { packageDirectory } from 'pkg-dir';
+
+import type { Options } from 'tsup';
+import { defineConfig } from 'tsup';
+
+export default defineConfig(async (): Promise<Options[]> => {
+  const PACKAGE_DIR = await packageDirectory();
+  const OUTPUT_DIR = path.resolve(PACKAGE_DIR, 'dist');
+
+  return [
+    {
+      clean: true,
+      entry: {
+        client: path.resolve(PACKAGE_DIR, 'src/index.tsx'),
+      },
+      format: 'cjs',
+      metafile: true,
+      minify: false,
+      noExternal: [/@comic\/.*/],
+      outDir: OUTPUT_DIR,
+      shims: true,
+      sourcemap: true,
+      splitting: false,
+      target: 'node18',
+      treeshake: false,
+    },
+  ];
+});
